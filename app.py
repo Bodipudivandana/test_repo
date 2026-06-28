0f32196f1475ed04906cad81f259489ed1664c5b
Bodipudivandana <bodipudivandanachowdary@gmail.com>
2026-06-28 19:22:01 +0530

    adding calculation code

---
+++ app.py
@@ -0,0 +1,16 @@
+#Generate code for simple calculation of addition, subtraction, multiplication and division 
+
+def add(x, y):
+    return x + y
+
+def subtract(x, y):
+    return x - y
+
+def multiply(x, y):
+    return x * y
+
+def divide(x, y):
+    if y == 0:
+        return "Error! Division by zero is not allowed."
+    else:
+        return x / y
